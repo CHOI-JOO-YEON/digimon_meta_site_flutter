a31806b81c8ec9df84cf5ab1afd44ca147de3911
CHOI-JOO-YEON <genboyjy@gmail.com>
2024-05-15 17:47:28 +0900

    fix: 덱 이름 텍스트 크기 수정

--- lib/widget/deck/builder/deck_menu_bar.dart
+++ lib/widget/deck/builder/deck_menu_bar.dart
@@ -1,24 +1,24 @@
-
 import 'package:digimon_meta_site_flutter/model/deck_response_dto.dart';
 import 'package:digimon_meta_site_flutter/widget/deck/deck_count_widget.dart';
 import 'package:flutter/material.dart';
 
 import '../../../model/deck.dart';
+
 class DeckBuilderMenuBar extends StatefulWidget {
   final Deck deck;
   final TextEditingController textEditingController;
 
-  const DeckBuilderMenuBar(
-      {super.key,
-      required this.deck, required this.textEditingController,
-      });
+  const DeckBuilderMenuBar({
+    super.key,
+    required this.deck,
+    required this.textEditingController,
+  });
 
   @override
   State<DeckBuilderMenuBar> createState() => _DeckBuilderMenuBarState();
 }
 
 class _DeckBuilderMenuBarState extends State<DeckBuilderMenuBar> {
-
   @override
   void didUpdateWidget(covariant DeckBuilderMenuBar oldWidget) {
     super.didUpdateWidget(oldWidget);
@@ -42,13 +42,13 @@ class _DeckBuilderMenuBarState extends State<DeckBuilderMenuBar> {
 
   @override
   Widget build(BuildContext context) {
-
     return LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
+      final fontSize = constraints.maxHeight * 0.15; // 텍스트 크기를 높이의 10%로 설정
       return Column(
         children: [
           Expanded(
-              flex: 1,
+              flex: 3,
               child: Padding(
                 padding: const EdgeInsets.all(0),
                 child: Container(
@@ -57,6 +57,7 @@ class _DeckBuilderMenuBarState extends State<DeckBuilderMenuBar> {
                     children: [
                       Expanded(
                           child: TextField(
+                        style: TextStyle(fontSize: fontSize),
                         controller: widget.textEditingController,
                         onChanged: (v) {
                           widget.deck.deckName = v;
@@ -66,9 +67,13 @@ class _DeckBuilderMenuBarState extends State<DeckBuilderMenuBar> {
                   ),
                 ),
               )),
-          Expanded(child: SizedBox(
-              width: constraints.maxWidth,
-              child: DeckCount(deck: widget.deck,)))
+          Expanded(
+              flex: 4,
+              child: SizedBox(
+                  width: constraints.maxWidth,
+                  child: DeckCount(
+                    deck: widget.deck,
+                  )))
         ],
       );
     });
